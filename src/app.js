b8c6cf4bccf0afb77c72de8371c8aa22df987114
ComputerEnjoyer <computerenjoyer01@gmail.com>
2023-06-04 13:17:19 +0200

    Set up app for Render

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const geocode = require('./utils/geocode');
 
 // Instantiate the server
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicDir = path.join(__dirname, '../public');
@@ -97,6 +98,6 @@ app.get('*', (req, res) => {
 });
 
 // Start the server on port 3000
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.');
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`);
 });
